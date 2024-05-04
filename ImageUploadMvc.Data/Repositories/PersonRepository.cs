bd3138491332d4ca04a6af49039b3405f5a1dec9
Ravindra Devrani <ravindra.devrani003@gmail.com>
2024-05-04 16:01:52 +0530

    Add project files.

---
+++ ImageUploadMvc.Data/Repositories/PersonRepository.cs
@@ -0,0 +1,71 @@
+﻿using Dapper;
+using ImageUploadMvc.Data.Models;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System.Data;
+
+namespace ImageUploadMvc.Data.Repositories;
+
+public interface IPersonRepository
+{
+    Task AddPerson(Person person);
+    Task DeletePerson(int id);
+    Task<IEnumerable<Person>> GetPeople();
+    Task<Person?> GetPersonById(int id);
+    Task UpdatePerson(Person person);
+}
+
+public class PersonRepository : IPersonRepository
+{
+    private readonly IConfiguration _configuration;
+    private string _connectionString;
+
+    public PersonRepository(IConfiguration configuration)
+    {
+        _configuration = configuration;
+        _connectionString = _configuration.GetConnectionString("default");
+    }
+
+    public async Task AddPerson(Person person)
+    {
+        using IDbConnection connection = new SqlConnection(_connectionString);
+        string sql = "insert into Person(FirstName,LastName,ProfilePicture) values(@FirstName,@LastName,@ProfilePicture)";
+        await connection.ExecuteAsync(sql, new
+        {
+            FirstName = person.FirstName,
+            LastName = person.LastName,
+            ProfilePicture = person.ProfilePicture,
+        });
+    }
+
+    public async Task UpdatePerson(Person person)
+    {
+        using IDbConnection connection = new SqlConnection(_connectionString);
+        string sql = "update Person set FirstName=@FirstName,LastName=@LastName,ProfilePicture=@ProfilePicture where Id=@Id";
+        await connection.ExecuteAsync(sql, person);
+    }
+
+    public async Task DeletePerson(int id)
+    {
+        using IDbConnection connection = new SqlConnection(_connectionString);
+        string sql = "delete from Person where id=@id";
+        await connection.ExecuteAsync(sql, new { id });
+    }
+
+    public async Task<Person?> GetPersonById(int id)
+    {
+        using IDbConnection connection = new SqlConnection(_connectionString);
+        string sql = "select * from Person where id=@id";
+        var person = await connection.QueryFirstOrDefaultAsync<Person>(sql, new { id });
+        return person;
+    }
+
+    public async Task<IEnumerable<Person>> GetPeople()
+    {
+        using IDbConnection connection = new SqlConnection(_connectionString);
+        string sql = "select * from Person";
+        var person = await connection.QueryAsync<Person>(sql);
+        return person;
+    }
+
+}
---
+++ ImageUploadMvc.Data/Shared/FileService.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+
+namespace ImageUploadMvc.Data.Shared;
+
+public interface IFileService
+{
+    void DeleteFile(string fileName, string directory);
+    Task<string> SaveFile(IFormFile file, string directory, string[] allowedExtensions);
+}
+
+public class FileService : IFileService
+{
+    private readonly IWebHostEnvironment _webHostEnvironment;
+    public FileService(IWebHostEnvironment webHostEnvironment)
+    {
+        _webHostEnvironment = webHostEnvironment;
+    }
+
+    public async Task<string> SaveFile(IFormFile file, string directory, string[] allowedExtensions)
+    {
+        var wwwPath = _webHostEnvironment.WebRootPath;
+        var path = Path.Combine(wwwPath, directory); // wwwroot/images
+        if (!Directory.Exists(path))
+        {
+            Directory.CreateDirectory(path);
+        }
+        // check the extesion is allowed or not
+        var extension = Path.GetExtension(file.FileName);
+        if (!allowedExtensions.Contains(extension))
+        {
+            throw new InvalidOperationException($"Only {string.Join(",", allowedExtensions)} extensions are allowed");
+        }
+        // create the unique file name
+        var newFileName = $"{Guid.NewGuid()}{extension}";  //asdfasd-sdfjadksfj-dda.jpeg
+        // create fullPath = path+newFileName
+        var fullPath = Path.Combine(path, newFileName);
+        // create a filestream
+        using var fileStream = new FileStream(fullPath, FileMode.Create);
+        await file.CopyToAsync(fileStream);
+        return newFileName;
+    }
+
+
+    public void DeleteFile(string fileName, string directory)
+    {
+        var fullPath = Path.Combine(_webHostEnvironment.WebRootPath, directory, fileName);
+        if (!Path.Exists(fullPath))
+        {
+            throw new FileNotFoundException($"File {fileName} does not exists");
+        }
+        File.Delete(fullPath);
+    }
+}
