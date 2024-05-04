6dccfa0e2d9ab7d4cd868e0c9c08ef3c90cc5cb1
Ravindra Devrani <ravindra.devrani003@gmail.com>
2024-05-04 16:04:56 +0530

    feat: empty controller

---
+++ ImageUploadMvc.UI/Controllers/PersonController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ImageUploadMvc.UI.Controllers;
+
+public class PersonController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
